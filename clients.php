aac452c1eff66f11cbbb0cdb2b960e44e60a951e
naziha <hamdi_naziha@yahoo.com>
2019-09-17 14:56:59 +0200

    new

--- clients.php
+++ clients.php
@@ -13,7 +13,8 @@ $dbname = "projetbancaire";}
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);	
+    die("Connection failed: " . $conn->connect_error);
+	
 }
 
 $sql = "SELECT id, nom, datedenaissance FROM client";
@@ -22,10 +23,11 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo " - Id: " . $row["id"]. " - Nom: " . $row["nom"]. " - Date de naissance: " . $row["datedenaissance"]. "<br>"; 
+        echo " - id: " . $row["id"]. " - Nom: " . $row["nom"]. " - Date de naissance: " . $row["datedenaissance"]. "<br>";
+		
     }
 } else {
-    echo "0 results";
+    echo "0 resultats";
 }
 $conn->close();
 ?>
