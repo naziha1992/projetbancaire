8d1cfac84d2b2531dbf28801bc3589be1433a349
naziha <hamdi_naziha@yahoo.com>
2019-09-17 14:43:49 +0200

    MAJ

--- clients.php
+++ clients.php
@@ -4,21 +4,16 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "projetbancaire";
-
-}else{
-	
+}else{	
 $servername = "172.17.0.2";
 $username = "root";
 $password = "";
-$dbname = "projetbancaire";
-}
-
+$dbname = "projetbancaire";}
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-	
+    die("Connection failed: " . $conn->connect_error);	
 }
 
 $sql = "SELECT id, nom, datedenaissance FROM client";
@@ -27,8 +22,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo " - Id: " . $row["id"]. " - Nom: " . $row["nom"]. " - Date de naissance: " . $row["datedenaissance"]. "<br>";
-	    
+        echo " - Id: " . $row["id"]. " - Nom: " . $row["nom"]. " - Date de naissance: " . $row["datedenaissance"]. "<br>"; 
     }
 } else {
     echo "0 results";
