9e046caa5d58845a7f3404ac0c51a59933acb604
naziha <hamdi_naziha@yahoo.com>
2019-09-18 11:13:43 +0200

    maj

--- addclient.php
+++ addclient.php
@@ -1,4 +1,8 @@
 <?php
-echo $_POST['nom']."<br>";
-echo $_POST['dn'];
+$nom=$_POST['nom'];
+$dn=$_POST['dn'];
+$link=mysqli_connect("localhost","root","","projetbancaire");
+mysqli_query($link, "INSERT INTO client value(0,\"".$nom."\",\"".$dn."\")");
+$SQL="insert into client values (0,\"".$nom."\",\"".$dn."\")";
+echo $SQL;
 ?>
