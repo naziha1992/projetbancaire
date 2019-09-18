f86d3719f9e48e2e0b54c403a34b62fca6fc89ff
stagiaire <stagiaire@localhost.localdomain>
2019-09-18 15:26:20 +0200

    test me

--- addclient.php
+++ addclient.php
@@ -1,8 +1,7 @@
 <?php
 $nom=$_POST['nom'];
 $dn=$_POST['dn'];
-$link=mysqli_connect("localhost","root","","projetbancaire");
-mysqli_query($link, "INSERT INTO client value(0,\"".$nom."\",\"".$dn."\")");
-$SQL="insert into client values (0,\"".$nom."\",\"".$dn."\")";
-echo $SQL;
+$link=mysqli_connect("!72.17.0.2","root","","projetbancaire");
+mysqli_query($link, "INSERT INTO client values(0,\"".$nom."\",\"".$dn."\")");
+
 ?>
